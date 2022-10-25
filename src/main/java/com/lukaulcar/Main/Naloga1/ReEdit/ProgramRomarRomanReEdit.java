bf760e21c70596406062ccf91749f22f30549e27
lukaulcar <114558213+lukaulcar@users.noreply.github.com>
2022-10-25 22:32:16 +0200

    Update ProgramRomarRomanReEdit.java

--- src/main/java/com/lukaulcar/Main/Naloga1/ReEdit/ProgramRomarRomanReEdit.java
+++ src/main/java/com/lukaulcar/Main/Naloga1/ReEdit/ProgramRomarRomanReEdit.java
@@ -11,7 +11,7 @@ public class ProgramRomarRomanReEdit {
 
     public static void main(String[] args) {
         int D = new Scanner(System.in).nextInt(), P = new Scanner(System.in).nextInt(), z, cph = 0, kppd = P, dpp, d = 1;
-        while (D > P && D > (cph += (dpp = kppd - (z = d == 1 ? 0 : (z = new Scanner(System.in).nextInt()) < 0 ? -z : z))) && (kppd -= z) > 0)
+        while ((D > P && D > 0 && P > 0) && D > (cph += (dpp = kppd - (z = d == 1 ? 0 : (z = new Scanner(System.in).nextInt()) < 0 ? -z : z))) && (kppd -= z) > 0)
             System.err.println(d++ + " dan: " + (D - (cph - dpp)) + " -> " + (D - cph) + " (prehodil " + dpp + ")");
     }
 
