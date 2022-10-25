404ce0b6c6397be00dbec4709ff2ee7513259379
lukaulcar <114558213+lukaulcar@users.noreply.github.com>
2022-10-25 21:17:50 +0200

    Update ProgramRomarRomanReEdit.java

--- src/main/java/com/lukaulcar/Main/Naloga1/ReEdit/ProgramRomarRomanReEdit.java
+++ src/main/java/com/lukaulcar/Main/Naloga1/ReEdit/ProgramRomarRomanReEdit.java
@@ -1,4 +1,4 @@
-package com.lukaulcar.Main.Naloga1.ReEdit;
+package com.lukaulcar.Naloga1.Main;
 
 import java.util.Scanner;
 
@@ -10,8 +10,8 @@ public class ProgramRomarRomanReEdit {
      */
 
     public static void main(String[] args) {
-        int D = new Scanner(System.in).nextInt(), P = new Scanner(System.in).nextInt(), z, cph = 0, kppd = P, dpp, d = 1, k = 1;
-        while (D > P && D > (cph += (dpp = kppd - (z = k-- > 0 ? 0 : (z = new Scanner(System.in).nextInt()) < 0 ? -z : z))) && (kppd -= z) > 0)
+        int D = new Scanner(System.in).nextInt(), P = new Scanner(System.in).nextInt(), z, cph = 0, kppd = P, dpp, d = 1;
+        while (D > P && D > (cph += (dpp = kppd - (z = d == 1 ? 0 : (z = new Scanner(System.in).nextInt()) < 0 ? -z : z))) && (kppd -= z) > 0)
             System.err.println(d++ + " dan: " + (D - (cph - dpp)) + " -> " + (D - cph) + " (prehodil " + dpp + ")");
     }
 
